6bdac481c778146a55404151f51f022d7ac4ae62
Tito <tito13kfm@gmail.com>
2022-05-07 12:59:12 -0400

    Code cleanup

--- IT1050 - Final/Program.cs
+++ IT1050 - Final/Program.cs
@@ -11,13 +11,12 @@
             var A2 = new Passenger("A2", 220);
             elevator1.AddOccupant(new Passenger("A1", 180), 0);
             elevator1.AddOccupant(new Passenger("A2", 220), 1);
-
-            bool elevator1IsOverMaxCapacity = elevator1.IsOverMaxCapacity();
             
             elevator2.AddOccupant(new Passenger("A1", 200), 0);
             elevator2.AddOccupant(new Passenger("A2", 200), 1);
             elevator2.AddOccupant(new Passenger("A3", 201), 2);
 
+            bool elevator1IsOverMaxCapacity = elevator1.IsOverMaxCapacity();
             bool elevator2IsOverMaxCapacity = elevator2.IsOverMaxCapacity();
         }
     }
